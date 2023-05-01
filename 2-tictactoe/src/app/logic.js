c49defb457f5ce02b75c41a532ba5067e8451611
Raul Escabia <raulescabia.work@gmail.com>
2023-05-01 18:02:21 +0200

    aded row, column and diagonal check

--- 2-tictactoe/src/app/logic.js
+++ 2-tictactoe/src/app/logic.js
@@ -1,13 +1,43 @@
 export function gameOutcome(grid) {
-  let outcome = null;
+  const boardArray = Object.values(grid);
 
-  //X wins
-  //O wins
-  //draw
-  console.log(Object.values(grid));
-  if (!Object.values(grid).some(({ state }) => state === "")) {
-    outcome = "draw";
+  function compareThree(v1, v2, v3) {
+    return grid[v1].state === grid[v2].state &&
+      grid[v2].state === grid[v3].state
+      ? grid[v1].state
+      : null;
   }
 
-  return outcome;
+  //X wins or O wins
+  /*
+    check rows 123 456 789
+    check columns 147 258 369
+    check diagonals 159 357
+  */
+  for (let i = 1; i <= 9; i = i + 3) {
+    //check row
+    const rowCheck = compareThree(i, i + 1, i + 2);
+    if (rowCheck) {
+      return "player " + rowCheck + " wins";
+    }
+  }
+  for (let i = 1; i <= 3; i++) {
+    //check column
+    const columnCheck = compareThree(i, i + 3, i + 6);
+    if (columnCheck) {
+      return "player " + columnCheck + " wins";
+    }
+  }
+  //check diagonals
+  const diagonal1Check = compareThree(1, 5, 9);
+  if (diagonal1Check) return "player " + diagonal1Check + " wins";
+  const diagonal2Check = compareThree(3, 5, 7);
+  if (diagonal2Check) return "player " + diagonal2Check + " wins";
+
+  //draw
+  if (!boardArray.some(({ state }) => state === "")) {
+    return "draw";
+  }
+
+  return null;
 }
