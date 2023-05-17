9548866c8b00aa8e7cbefd8e6895154eff5f5595
Raul <raulescabia.work@gmail.com>
2023-05-17 12:05:09 +0200

    update styling

--- 06-shopping-cart/src/components/Product.js
+++ 06-shopping-cart/src/components/Product.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Product({ image, title, price, category }) {
   return (
-    <article>
+    <article className="product-item">
       <h3>{title}</h3>
       <img src={image} />
       <p>
